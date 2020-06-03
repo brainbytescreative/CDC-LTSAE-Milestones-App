c2cc7a9126b17fb4dfc347e2b3d4dcfd644a2da8
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2020-06-03 17:25:18 +0300

    CDCRN-100 - Android: Current month should be centered

--- src/screens/Dashboard/MonthCarousel.tsx
+++ src/screens/Dashboard/MonthCarousel.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useLayoutEffect, useMemo, useRef} from 'react';
-import {FlatList, FlatListProps, Text, TouchableOpacity, View} from 'react-native';
+import {FlatList, FlatListProps, Platform, Text, TouchableOpacity, View} from 'react-native';
 import {AnimatedCircularProgress} from 'react-native-circular-progress';
 import {childAges, colors, PropType} from '../../resources/constants';
 import {useTranslation} from 'react-i18next';
@@ -73,25 +73,21 @@ const MonthCarousel: React.FC = () => {
 
   const {data: child} = useGetCurrentChild();
 
-  const currentAgeIndex = childAges.findIndex((value) => value === childAge);
+  const currentAgeIndex = childAges.findIndex((value) => value === milestoneAge);
 
   useLayoutEffect(() => {
     setTimeout(() => {
-      try {
-        if (currentAgeIndex > -1) {
-          flatListRef.current?.scrollToIndex({
-            index: currentAgeIndex,
-            viewPosition: 0.5,
-          });
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }, 500);
+      flatListRef.current?.scrollToIndex({
+        index: currentAgeIndex,
+        viewPosition: 0.5,
+      });
+    }, Platform.select({default: 0, android: 500}));
   }, [currentAgeIndex]);
 
   const onViewableItemsChanged = useCallback<onViewableItemsChanged>((info) => {
-    visible.current = {first: _.first(info.viewableItems)?.index, last: _.last(info.viewableItems)?.index};
+    const first = _.first(info.viewableItems)?.index;
+    const last = _.last(info.viewableItems)?.index;
+    visible.current = {first, last};
   }, []);
 
   // workaround to fix crashes on fast refresh
@@ -120,6 +116,14 @@ const MonthCarousel: React.FC = () => {
         ref={flatListRef}
         style={{
           marginHorizontal: 13,
+        }}
+        onScrollToIndexFailed={() => {
+          setTimeout(() => {
+            flatListRef.current?.scrollToIndex({
+              index: currentAgeIndex,
+              viewPosition: 0.5,
+            });
+          }, 1000);
         }}
         onViewableItemsChanged={onViewableItemsChanged}
         data={childAges}
