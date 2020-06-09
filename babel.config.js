e57bf19c3abd51e2e488a879cada53b4c7248e2a
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2020-06-09 13:34:51 +0300

    bundle optimisation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: [
+        'react-native-paper/babel',
+        'date-fns',
+        'transform-react-remove-prop-types',
+        'transform-remove-console',
+      ],
+    },
+  },
 };
