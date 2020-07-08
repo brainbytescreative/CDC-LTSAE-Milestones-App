4c67244a218d133842ca7e6ee989d018ca87abad
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2020-07-08 12:25:22 +0300

    fix: CDCRN-129

--- src/components/Svg/PurpleArc.tsx
+++ src/components/Svg/PurpleArc.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Svg, {Path, SvgProps} from 'react-native-svg';
 
-const PurpleArc: React.FC<SvgProps> = (props) => (
-  <Svg width="100%" height="18px" viewBox="0 0 375 18" preserveAspectRatio="none" {...props}>
+const PurpleArc: React.FC<SvgProps> = ({style, ...props}) => (
+  <Svg
+    style={[style, {marginBottom: -1}]}
+    width="100%"
+    height="18px"
+    viewBox="0 0 375 18"
+    preserveAspectRatio="none"
+    {...props}>
     <Path
       d="M375.001 91.923v2.036H0v-1.861a1088.085 1088.085 0 01185.093-16.101H188a1088.87 1088.87 0 01187 15.926z"
       transform="translate(0 -76)"
