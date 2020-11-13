46b893fe373e56341d9866ee855f035422b5e1a5
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2020-11-13 09:14:42 +0200

    feat: wip on premature feature

--- src/components/AppStateManager.ts
+++ src/components/AppStateManager.ts
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import {NavigationContainerRef} from '@react-navigation/core';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 import {AppState, AppStateStatus} from 'react-native';
 import {queryCache} from 'react-query';
 
@@ -8,55 +9,71 @@ import {currentScreen, trackAppLaunch} from '../utils/analytics';
 
 type StateChangeListener = Parameters<PropType<AppState, 'addEventListener'>>[1];
 
+const showChildrenList = (navigation?: NavigationContainerRef | null) => {
+  console.log('showChildrenList');
+  navigation = navigation ?? currentScreen.navigation?.current;
+  navigation?.reset({
+    index: 0,
+    routes: [
+      {
+        name: 'DashboardStack',
+        state: {
+          index: 1,
+          routes: [
+            {
+              name: 'Dashboard',
+              params: {
+                addChild: true,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+};
+
+const hideChildrenList = (navigation?: NavigationContainerRef | null) => {
+  navigation = navigation ?? currentScreen.navigation?.current;
+  navigation?.reset({
+    index: 0,
+    routes: [
+      {
+        name: 'DashboardStack',
+        state: {
+          index: 1,
+          routes: [
+            {
+              name: 'Dashboard',
+              params: {
+                addChild: undefined,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+};
+
 const AppStateManager: React.FC = () => {
   const [appState, setAppState] = useState<AppStateStatus | undefined>();
   const [scheduleNotifications] = useScheduleNotifications();
+
+  useLayoutEffect(() => {
+    if (appState === undefined) {
+      setTimeout(showChildrenList);
+    }
+  }, [appState]);
 
   const _handleAppStateChange: StateChangeListener = (nextAppState) => {
     if (!!appState?.match(/inactive|background/) && nextAppState === 'active') {
       scheduleNotifications();
       queryCache.invalidateQueries('unreadNotifications', {refetchActive: true, refetchInactive: true});
       trackAppLaunch();
-
-      currentScreen.navigation?.current?.reset({
-        index: 0,
-        routes: [
-          {
-            name: 'DashboardStack',
-            state: {
-              index: 1,
-              routes: [
-                {
-                  name: 'Dashboard',
-                  params: {
-                    addChild: true,
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      });
+      showChildrenList();
     } else {
-      currentScreen.navigation?.current?.reset({
-        index: 0,
-        routes: [
-          {
-            name: 'DashboardStack',
-            state: {
-              index: 1,
-              routes: [
-                {
-                  name: 'Dashboard',
-                  params: {
-                    addChild: undefined,
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      });
+      hideChildrenList();
     }
     setAppState(nextAppState);
   };
